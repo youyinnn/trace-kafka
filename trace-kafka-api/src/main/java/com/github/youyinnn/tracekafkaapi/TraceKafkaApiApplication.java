c139520f5654f94e8ffe8ffb1a60127e6505c4f9
youyinnn <454947049@qq.com>
2019-04-22 15:32:33 +0800

    Init
    Init:
    1.trace-kafka
    2.trace-kafka-api
    3.trace-kafka-sample

---
+++ trace-kafka-api/src/main/java/com/github/youyinnn/tracekafkaapi/TraceKafkaApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.youyinnn.tracekafkaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraceKafkaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TraceKafkaApiApplication.class, args);
+    }
+
+}
---
+++ trace-kafka-sample/src/main/java/com/github/youyinnn/tracekafkasample/TraceKafkaSampleApplication.java
@@ -0,0 +1,16 @@
+package com.github.youyinnn.tracekafkasample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author youyinnn
+ */
+@SpringBootApplication
+public class TraceKafkaSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TraceKafkaSampleApplication.class, args);
+    }
+
+}
