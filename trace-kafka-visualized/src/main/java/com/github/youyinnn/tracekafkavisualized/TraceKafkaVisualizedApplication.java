3a57f91ec0969cd1e8972cbc2f8e927437d031e8
youyinnn <454947049@qq.com>
2019-05-12 21:15:30 +0800

    Update*

--- trace-kafka-visualized/src/main/java/com/github/youyinnn/tracekafkavisualized/TraceKafkaVisualizedApplication.java
+++ trace-kafka-visualized/src/main/java/com/github/youyinnn/tracekafkavisualized/TraceKafkaVisualizedApplication.java
@@ -2,8 +2,6 @@ package com.github.youyinnn.tracekafkavisualized;
 
 import com.github.youyinnn.youdbutils.YouDbManager;
 import com.github.youyinnn.youdbutils.druid.YouDruid;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
